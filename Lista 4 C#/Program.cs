d126bbfb8c310e3666514111e83de46fa7c63896
Renanzin <renan.santos17senai@gmail.com>
2024-06-01 21:31:14 -0300

    a

---
+++ Lista 4 C#/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+
+namespace Lista_4_C_
+{
+    class projeto
+    {
+        static void Main(string[] args)
+        {
+            /*int numero = 0;
+            while (numero <= 10) {
+            Console.WriteLine(numero);
+            numero++;
+            }*/
+
+            /* char resposta = char.Parse(Console.ReadLine());
+             while (resposta == 's')
+             {
+             Console.WriteLine("Desejar continuar? ");
+             resposta = char.Parse(Console.ReadLine());
+             }*/
+
+            /*for (int i = 0; i<= 10; i++)
+            {
+                Console.WriteLine(i);
+            }*/
+
+            /*int num = 5;
+            for(int i = 0; i <= 10; i++)
+            {
+                int resultado = num * i;
+                Console.WriteLine(resultado);
+                Console.WriteLine("");
+                Console.WriteLine(num + " x " + i + " = " + resultado);*/
+
+
+            /*{
+                Console.WriteLine("Informe um número para ver sua tabuada:");
+                int numero = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(numero * i);
+                }
+            }*/
+
+            /*{
+                Console.WriteLine("Digite um número:");
+                int numero = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i <= numero; i += 3)
+                {
+                    Console.WriteLine(i);
+                }
+            }*/
+
+            /*{
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Digite um número:");
+                    int numero = int.Parse(Console.ReadLine());
+
+                    if (numero > 100)
+                    {
+                        Console.WriteLine("NÚMERO ESPECIAL");
+                    }
+                    else
+                    {
+                        Console.WriteLine("NÚMERO COMUM");
+                    }
+                }
+            }*/
+
+            /*{
+                string resposta;
+                do
+                {
+                    Console.WriteLine("Deseja continuar? (s/n)");
+                    resposta = Console.ReadLine().ToLower();
+                } while (resposta == "s");
+            }*/
+        }
+    }
+}
